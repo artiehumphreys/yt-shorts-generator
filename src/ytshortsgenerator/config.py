dea88daaa502b48f519ef448a53659a74767de50
artiehumphreys <artieh2003@gmail.com>
2025-08-02 14:26:54 -0500

    feat(yt): youtube credential generator

--- src/ytshortsgenerator/config.py
+++ src/ytshortsgenerator/config.py
@@ -6,8 +6,9 @@ load_dotenv()
 
 @dataclass
 class Settings:
-	reddit_client_id: str = os.getenv("REDDIT_CLIENT_ID")
-	reddit_client_secret: str = os.getenv("REDDIT_CLIENT_SECRET")
-	reddit_user_agent: str = os.getenv("REDDIT_USER_AGENT", "ytshortsgenerator/0.1")
+    reddit_client_id: str = os.getenv("REDDIT_CLIENT_ID")
+    reddit_client_secret: str = os.getenv("REDDIT_CLIENT_SECRET")
+    reddit_user_agent: str = os.getenv("REDDIT_USER_AGENT", "ytshortsgenerator/0.1")
+    elevenlabs_api_key: str = os.getenv("ELEVENLABS_API_KEY")
 
 settings = Settings()
